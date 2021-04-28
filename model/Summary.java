49cbae5254120ba8d748abf14b4a629b10d3110c
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-04-28 15:39:02 +0200

    Summary.java

--- model/Summary.java
+++ model/Summary.java
@@ -1,5 +1,33 @@
 package model;
 
 public class Summary {
-    
+    private double runningTotal;
+    private double totalTax;
+    private double totalDiscount;
+    private double summary;
+
+    /**
+     * Get the total cost of the purchase
+     * @return the total cost of the purchase
+     */
+    public double getRunningTotal() { return this.runningTotal; }
+
+    /**
+     * Get the total amount of tax of the purchase
+     * @return  the total amount of tax
+     */
+    public double totalTax() { return this.totalTax; }
+
+    /**
+     * Get the total amount of discount of the purchase
+     * @return the total amount of discount
+     */
+    public double totalDiscount() { return this.totalDiscount; }
+
+    /**
+     * returns the total price before tax and discount
+     * @return total price of products
+     */
+    public double sum() { return this.summary; }
 }
+
