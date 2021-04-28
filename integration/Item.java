74912d7a101607edbc5db5712c440159ef6a9501
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-04-28 17:07:13 +0200

    Amount class fixed

--- integration/Item.java
+++ integration/Item.java
@@ -1,18 +1,19 @@
 package integration;
 
+import util.Amount;
 /**
  * Represents an Item
  */
 public class Item {
     private ItemDTO itemInformation;
-    private int itemQuantity;
+    private Amount itemQuantity;
 
     /**
      * Creates a new instance of an Item
      * @param itemInformation The information of an item
      * @param Quantity the amount of each item
      */
-    public Item(ItemDTO itemInformation, int Quantity) {
+    public Item(ItemDTO itemInformation, Amount Quantity) {
         this.itemInformation = itemInformation;
         this.itemQuantity = itemQuantity;
     }
