a3598c2d560cb02f32c9b0f5767dbbf196a3a0d0
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-04-29 16:05:27 +0200

    SalesLog created

--- integration/SalesLog.java
+++ integration/SalesLog.java
@@ -1,5 +1,23 @@
 package integration;
 
 public class SalesLog {
+    private SalesLog salesLog;
+    private Sale sale;
+
+    /**
+     * Creates an instance of a sales log
+     * 
+     * @param sale the sale information
+     */
+    public SalesLog(Sale sale) {
+        this.sale = sale;
+    }
+
+    /**
+     * Gets the salesLog 
+     * 
+     * @return return the value of the salesLog
+     */
+    public getSalesLog() { return salesLog; }
     
 }
