ed4ec2b90f64a0facf7c5eb4ecb574cf540572cb
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-04-30 15:59:43 +0200

    small smooth fixes in various classes

--- integration/DiscountCatalog.java
+++ integration/DiscountCatalog.java
@@ -2,6 +2,7 @@ package integration;
 
 public class DiscountCatalog {
     private double discount;
+    private boolean discountAvailability;
 
     //public double availableDiscount(int customerID) {}
     
