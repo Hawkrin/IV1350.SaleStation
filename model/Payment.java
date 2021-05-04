b35dc77e7d015b0c9b7b22a6a49c04f426441304
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2021-05-04 12:07:19 +0200

    payment + cashregister done

--- model/Payment.java
+++ model/Payment.java
@@ -6,15 +6,32 @@ import util.Amount;
  */
 public class Payment {
     private Amount paidAmount;
-    private Amount totalCost;
+    private Amount totalPrice;
 
     /**
      * Creates an instance, an amount has been used as payment
      * 
-     * @param paidAmount the amount of cash used to pay
+     * @param paidAmount the {@link Amount} of cash used to pay
      */
-    public Payment(Amount paidAmount){
+    public Payment(Amount paidAmount, Amount totalPrice){
         this.paidAmount = paidAmount;
+        this.totalPrice = totalPrice;
     }
+
+    /**
+     * Calculates the change to return
+     * 
+     * @return the change to return as a {@link Amount}
+     */
+    public Amount getChange() {
+        return paidAmount.subtract(totalPrice.getSummary());
+    }
+
+    /**
+     * Get the price of the purchase
+     * 
+     * @return the {@link Amount} of the purchase 
+     */
+    public Amount getSum(){ return totalPrice; }
     
 }
