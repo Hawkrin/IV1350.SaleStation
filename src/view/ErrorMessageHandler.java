4a132e7ad8dd5657c4bb1e8ca60cc378df556cca
Malcolm <malcolm.liljedahl94@gmail.com>
2021-05-15 12:53:29 +0200

    ErrorMessageHandler.java created

---
+++ src/view/ErrorMessageHandler.java
@@ -0,0 +1,23 @@
+package src.view;
+
+import java.time.LocalDateTime;
+
+/**
+ * This class shows the error messages to the user
+ */
+public class ErrorMessageHandler {
+    LocalDateTime timeRightNow = LocalDateTime.now();
+    
+ /**
+     * Displays the specified error message.
+     * 
+     * @param msg The error message to display.
+     */
+    void displayErrorMessage(String msg) {
+        StringBuilder occuredError = new StringBuilder();
+        occuredError.append(timeRightNow);
+        occuredError.append(", ERROR: ");
+        occuredError.append(msg);
+        System.out.println(occuredError);
+    }
+}
